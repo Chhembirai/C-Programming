2772d28fab203cad2cb11b46d7643bcd5325a2ad
Hank <raichhembihang85@gmail.com>
2026-01-30 22:45:40 +0545

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,9 @@
+//1
+//For showing simple structure like heading, comment.
+//This is comments.
+#include<stdio.h>
+int main(){
+    printf("Hello World\n");
+    printf("I am learning C programming language.\n");
+    return 0;
+}
---
+++ q2.c
@@ -0,0 +1,8 @@
+//2
+//Showing personal information
+#include<stdio.h>
+int main(){
+    printf("My name is Bikas B.K.\nI live in Kupondole, Lalitpur, Nepal.\n");
+    printf("I am studying BCA at\tPatan Campus\tin first semester.\nMy roll no. is 15.\n");
+    return 0;
+}
